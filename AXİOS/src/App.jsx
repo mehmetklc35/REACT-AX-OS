75c073f0b3577a581adc67a7c23fd556a741eae1
Your Name <you@example.com>
2025-02-09 18:39:47 +0300

    css düzenlemeleri yapıldı

--- AXİOS/src/App.jsx
+++ AXİOS/src/App.jsx
@@ -3,11 +3,13 @@ import Products from "./Components/Products";
 
 function App() {
   return (
-
     <div className="container mx-auto p-4">
       <Header />
-      
-      <input type="text" className="w-full p-2 border border-gray-300 rounded-lg mb-4" placeholder="Gelinlik ara..." />
+      <input
+        type="text"
+        className="w-full p-2 border border-gray-300 rounded-lg mb-4"
+        placeholder="Gelinlik ara..."
+      />
       <Products />
     </div>
   );
